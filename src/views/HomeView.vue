83a051e89135d0aee10e50cd6a3d151c39cd2a43
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-07-08 18:29:24 -0300

    construction: remove structure to start project

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    HomeView
+  </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: 'HomeView'
+})
+</script>
