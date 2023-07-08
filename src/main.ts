4be612c26069f519b6bd4a864321e9dbeb00e186
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-07-08 19:43:37 -0300

    feat: set tailwind into the project config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+
+// defining styles
+import './assets/styles/index.css';
 
 const app = createApp(App)
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="text-3xl font-bold underline">
     HomeView
   </div>
 </template>
